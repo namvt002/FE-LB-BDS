97d024088a3d39cdafb008debe7a1e98c85cb509
namvt002 <viettrung0601@gmail.com>
2022-10-05 11:24:34 +0700

    test google

--- server/app.js
+++ server/app.js
@@ -3,8 +3,8 @@ const morgan = require("morgan");
 const cors = require("cors");
 const CookieParser = require("cookie-parser");
 const session = require("express-session");
-// const passport = require("passport");
-// require("./lib/googleLogin");
+const passport = require("passport");
+require("./lib/googleLogin");
 const path = require("path");
 global.publicPath = path.resolve("public/images");
 const bodyParser = require("body-parser");
@@ -37,12 +37,47 @@ app.use(function (req, res, next) {
   next();
 });
 
+app.use(session({ secret: "cats", resave: true, saveUninitialized: true }));
+app.use(passport.initialize());
+app.use(passport.session());
 
+app.get(
+  "/auth/google",
+  passport.authenticate("google", { scope: ["email", "profile"] })
+);
 
+app.get(
+  "/auth/google/callback",
+  passport.authenticate("google", {
+    successRedirect: "/auth/login/google",
+    failureRedirect: "/auth/google/failure",
+  })
+);
+app.get("/logout", (req, res) => {
+  req.session.destroy();
+  req.logout();
+  res.clearCookie("connect.sid");
+  res.clearCookie("token");
+  res.clearCookie("email");
+  res.clearCookie("fullname");
+  return res.status(200).send("Goodbye!");
+});
+require("./routes/auth")(app);
+require("./routes/Users")(app);
+require("./routes/Role")(app);
+require("./routes/nhaxuatban")(app);
+require("./routes/nhacungcap")(app);
 require("./routes/danhmuc")(app);
-
-
-const PORT = process.env.PORT || PORT;
+require("./routes/tacgia")(app);
+require("./routes/theloai")(app);
+require("./routes/ngonngu")(app);
+require("./routes/book")(app);
+require("./routes/phieunhap")(app);
+app.get("/", (req, res) => {
+  res.send('<a href="/auth/google">Authenticate with Google</a>');
+});
+const PORT = process.env.PORT || 4000;
+console.log(process.env.PORT)
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
 });
